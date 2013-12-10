286d14a3e0909729c9e2aefd6ac1e9691861c14f
Stefan Adamov <stefan.adamof@gmail.com>
2013-12-10 14:51:27 +0000

    stop redis reporting

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 require_relative 'init'
 
 Rufus::TrackingScheduler.start do |scheduler|
-  scheduler.run :name => 'redis_information_push', :every => '60s' do
-    Watchdog::RedisMonitor.run
-  end
+  # scheduler.run :name => 'redis_information_push', :every => '60s' do
+  #   Watchdog::RedisMonitor.run
+  # end
   scheduler.run :name => 'mongo_information_push', :every => '60s' do
     Watchdog::MongoMonitor.run
   end
--- lib/watchdog/mongo_monitor.rb
+++ lib/watchdog/mongo_monitor.rb
@@ -3,6 +3,7 @@ module Watchdog
     def self.run
       info = MongoConnection.db.stats
       info = InformationFilter.new('mongo', info).relevant_only
+      puts info
       DataDogPusher.new.push('mongo', info)
     end
   end
