bfec51912c53b12311ea55dfd9b8b4cdd01b51b1
Stefan Adamov <stefan.adamof@gmail.com>
2013-12-10 15:06:00 +0000

    Puts back redis monitoring

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,11 @@
 require_relative 'init'
 
 Rufus::TrackingScheduler.start do |scheduler|
-  # scheduler.run :name => 'redis_information_push', :every => '60s' do
-  #   Watchdog::RedisMonitor.run
-  # end
-  scheduler.run :name => 'mongo_information_push', :every => '60s' do
+  scheduler.run :name => 'redis_information_push', :every => '60s' do
+    Watchdog::RedisMonitor.run
+  end
+
+  scheduler.run :name => 'mongo_information_push', :every => '5m' do
     Watchdog::MongoMonitor.run
   end
 end
--- lib/watchdog/shared/datadog_pusher.rb
+++ lib/watchdog/shared/datadog_pusher.rb
@@ -1,11 +1,9 @@
 module Watchdog
   class DataDogPusher
-
     def push(type, data)
       data.each_pair do |metric, value|
         DataDogClient.emit_point("#{type}.#{metric}", value)
       end
     end
-
   end
 end
