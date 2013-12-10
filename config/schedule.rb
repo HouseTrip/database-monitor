c74fce3e2db75c2a70b85a9c7ae8f96fdf6d09df
Stefan Adamov <stefan.adamof@gmail.com>
2013-12-10 14:36:15 +0000

    Change the reporting to every 1 minute

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@ Rufus::TrackingScheduler.start do |scheduler|
   scheduler.run :name => 'redis_information_push', :every => '60s' do
     Watchdog::RedisMonitor.run
   end
-  scheduler.run :name => 'mongo_information_push', :every => '5m' do
+  scheduler.run :name => 'mongo_information_push', :every => '60s' do
     Watchdog::MongoMonitor.run
   end
 end
